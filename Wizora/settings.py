d7d66b65891d45ff793c1aca3cab01629f5d0709
Harsh Dave <daveharsh38@gmail.com>
2025-05-05 23:56:25 +0530

    ✅ Integrated Zoho SMTP email support with .env for Wizora

--- Wizora/settings.py
+++ Wizora/settings.py
@@ -140,8 +140,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # SMTP Email settings (example using Gmail SMTP)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
